444c45704695eac92c4d1269ed01447f229be2c8
Ariany Alves <ariariany093@gmail.com>
2025-08-17 20:28:37 -0300

    add file: ex09

---
+++ revisao-python/ex09.py
@@ -0,0 +1,15 @@
+import requests
+
+sigla = input("Digite a Sigla da moeda que voocê deseja fazer a conversão para real. \nPor exemplo: USD (Dólar), EUR (Euro), GBD (Libra Esterlina)\n").upper()
+
+url = f"https://api.exchangerate-api.com/v4/latest/BRL"
+
+response = requests.get(url)
+data = response.json()
+
+if sigla in data["rates"]:
+    cambio = data["rates"][sigla]
+    moeda_convertida = 1/cambio
+    print(f"O Real vale {moeda_convertida:.2f}")
+else:
+    print("Sigla não encontrada! Por favor, digite uma nova sigla")
